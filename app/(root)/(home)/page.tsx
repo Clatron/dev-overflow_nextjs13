313e0ea7c1e8141b19829689543805e24fd98f13
Clatron <bariskaya1516@gmail.com>
2023-10-21 21:51:31 +0300

    Add auth

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,6 +1,9 @@
-const Home = () => {
+import { UserButton } from "@clerk/nextjs";
+ 
+export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
   )
 }
-export default Home
