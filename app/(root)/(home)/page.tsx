1b505edbb92b199685e09583793a9b95147d983d
Clatron <bariskaya1516@gmail.com>
2023-10-23 13:56:25 +0300

    add layout

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,6 +3,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div>
+      <h1 className=" h1-bold">Piece of text</h1>
       <UserButton afterSignOutUrl="/"/>
     </div>
   )
