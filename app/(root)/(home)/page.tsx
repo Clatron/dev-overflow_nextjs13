6bd831191d3dce775d916188942b22430a28ec28
Clatron <bariskaya1516@gmail.com>
2023-10-29 11:22:17 +0300

    add navbar and theme context

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,8 @@
-import { UserButton } from "@clerk/nextjs";
  
 export default function Home() {
   return (
     <div>
       <h1 className=" h1-bold">Piece of text</h1>
-      <UserButton afterSignOutUrl="/"/>
     </div>
   )
 }
