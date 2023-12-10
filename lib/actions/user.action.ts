59ff65a8d411a587daacff1e79eee46940e783c3
Clatron <bariskaya1516@gmail.com>
2023-12-10 19:52:24 +0300

    fix build errors

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -67,8 +67,7 @@ export async function deleteUser(params: DeleteUserParams) {
     // Questions, Answers, Comments, etc.
 
     // Get user question ids
-    const userQuestionIds = await Question.find({ author: user._id })
-      .distinct('_id')
+    // const userQuestionIds = await Question.find({ author: user._id }).distinct('_id')
 
     // Delete user questions
     await Question.deleteMany({ author: user._id })
